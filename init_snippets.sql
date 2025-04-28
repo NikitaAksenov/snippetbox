64238a4904848df651e156b51ccd21ff24cc4276
NikitaAksenov <naksenov96@gmail.com>
2025-04-28 21:58:12 +0300

    Chapter 4.1

---
+++ init_snippets.sql
@@ -0,0 +1,22 @@
+DELETE FROM snippets;
+
+INSERT INTO snippets (title, content, created, expires) VALUES (
+    'An old silent pond',
+    'An old silent pond...\nA frog jumps into the pond,\nsplash! Silence again.\n\n– Matsuo Bashō',
+    datetime('now'),
+    datetime('now', '1 years')
+);
+
+INSERT INTO snippets (title, content, created, expires) VALUES (
+    'Over the wintry forest',
+    'Over the wintry\nforest, winds howl in rage\nwith no leaves to blow.\n\n– Natsume Soseki',
+    datetime('now'),
+    datetime('now', '1 years')
+);
+
+INSERT INTO snippets (title, content, created, expires) VALUES (
+    'First autumn morning',
+    'First autumn morning\nthe mirror I stare into\nshows my father''s face.\n\n– Murakami Kijo',
+    datetime('now'),
+    datetime('now', '7 days')
+);

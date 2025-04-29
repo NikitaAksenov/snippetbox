5cfb1082973d82d1fe86d7b788009dd5d0e9bc1e
NikitaAksenov <naksenov96@gmail.com>
2025-04-29 20:30:11 +0300

    Chapter 4.4

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type application struct {
@@ -14,10 +17,20 @@ type application struct {
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "snippetbox.db", "sqlite3 data source name")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := sql.Open("sqlite3", *dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	if err = db.Ping(); err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
 
 	app := &application{
 		infoLog:  infoLog,
@@ -31,6 +44,6 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s\n", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
 }

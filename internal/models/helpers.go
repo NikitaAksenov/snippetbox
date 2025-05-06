ccfb1298174d0be2e0bb674674416d02e52e96b6
NikitaAksenov <naksenov96@gmail.com>
2025-05-06 21:17:19 +0300

    Chapter 4.8

--- internal/models/helpers.go
+++ internal/models/helpers.go
@@ -5,11 +5,35 @@ import (
 	"time"
 )
 
+const DateTime string = "2006-01-02 15:04:05"
+
 func ParseRowToSnippet(row *sql.Row) (*Snippet, error) {
 	snippet := Snippet{}
 
 	var createdStr, expiresStr string
 	err := row.Scan(&snippet.ID, &snippet.Title, &snippet.Content, &createdStr, &expiresStr)
+	if err != nil {
+		return nil, err
+	}
+
+	snippet.Created, err = time.Parse(DateTime, createdStr)
+	if err != nil {
+		return nil, nil
+	}
+
+	snippet.Expires, err = time.Parse(DateTime, expiresStr)
+	if err != nil {
+		return nil, nil
+	}
+
+	return &snippet, nil
+}
+
+func ParseRowsToSnippet(rows *sql.Rows) (*Snippet, error) {
+	snippet := Snippet{}
+
+	var createdStr, expiresStr string
+	err := rows.Scan(&snippet.ID, &snippet.Title, &snippet.Content, &createdStr, &expiresStr)
 	if err != nil {
 		return nil, err
 	}
--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -41,8 +41,6 @@ func (m *SnippetModel) Insert(title string, content string, expires int) (int, e
 	return int(lastID), err
 }
 
-const DateTime string = "2006-01-02 15:04:05"
-
 func (m *SnippetModel) Get(id int) (*Snippet, error) {
 	query := `
 	SELECT id, title, content, created, expires
@@ -65,5 +63,35 @@ func (m *SnippetModel) Get(id int) (*Snippet, error) {
 }
 
 func (m *SnippetModel) Latest(count int) ([]*Snippet, error) {
-	return nil, nil
+	query := `
+	SELECT id, title, content, created, expires
+		FROM snippets
+		WHERE datetime('now') < expires
+		ORDER BY id DESC
+		LIMIT ?
+	;`
+
+	rows, err := m.DB.Query(query, count)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	latestSnippets := make([]*Snippet, 0, 10)
+
+	for rows.Next() {
+		snippet, err := ParseRowsToSnippet(rows)
+		if err != nil {
+			return nil, err
+		}
+
+		latestSnippets = append(latestSnippets, snippet)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		return nil, err
+	}
+
+	return latestSnippets, nil
 }
